7036890a2e59caa96b8ac7dff4dfc43676c26216
Sunita Gurau <sunita.gurau@introcept.co>
2022-06-12 12:39:10 +0545

    detail page added

--- js/movie.js
+++ js/movie.js
@@ -1,31 +1,43 @@
 let response= fetch("https://yts.mx/api/v2/list_movies.json")
-.then((data) => {
-    return data.json();
-}).then((completeData) => {
-    console.log(completeData);
+.then((data) =>data.json())
+.then(({data}) => {
+    console.log(data);
     let data1="";
-    completeData.map((values) => {
+    data.movies.map((values) => {
         data1+=`<div>
-        <a href="#" class="relative z-1 ">
-            <img src="images/movie1.jpg" class="border-4 border-white hover:border-green-500" alt="movie">
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-                <div class="text-center">
-                    <i class="fa-solid fa-star text-green-500 text-3xl pb-3 "></i>
-                    <p class="text-white text-2xl font-bold">${values.genres}</p>
-                    <p class="text-white text-2xl font-bold pt-5">Action</p>
-                    <p class="text-white text-2xl font-bold">Comedy</p>
-                    <button class="bg-green-500 text-white text-sm px-2 py-2 mt-5 font-semibold rounded">View Details</button>
-                </div>
+        <div
+          class="border-4 border-white hover:border-green-500 relative group"
+        >
+          <a href="review.html">
+            <img src="${values.medium_cover_image}" alt="${values.title}" />
+          </a>
+          <div
+            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+          >
+            <div
+              class="text-center hidden group-hover:block hover:transition-all hover:duration-500 hover:delay-300 hover:ease-in-out"
+            >
+              <i class="fa-solid fa-star text-green-500 text-3xl pb-3"></i>
+              <p class="text-white text-2xl font-bold">${values.rating}</p>
+              <p class="text-white text-2xl font-bold pt-5">${values.genres[0]}</p>
+              <p class="text-white text-2xl font-bold">${values.genres[1]}</p>
+              <button
+                class="bg-green-500 text-white text-sm px-2 py-2 mt-5 font-semibold rounded"
+              >
+                View Details
+              </button>
             </div>
-        </a>
-        <div class="pl-2">
-            <a href="#" class="text-white text-lg font-bold">Partner</a>
-            <p class="text-neutral-300 text-sm">2007</p>
+          </div>
         </div>
-    </div>`;
+
+        <div class="pl-2">
+          <a href="#" class="text-white text-lg font-bold">${values.title}</a>
+          <p class="text-neutral-300 text-sm">${values.year}</p>
+        </div>
+      </div>`;
     });
    document.getElementById("Movie1").innerHTML=data1;
     
-}).catch((error) => {
+}).catch((error) =>{
     console.log("Unable to fetch data",error);
 })
---
+++ js/review.js
@@ -0,0 +1,290 @@
+let response= fetch("https://yts.mx/api/v2/list_movies.json")
+.then((data) =>data.json())
+.then(({data}) => {
+    console.log(data);
+    let data1="";
+    data.movies.map((values) => {
+        data1=`<section class="mx-48 my-8 flex">
+        <div class="w-1/4 mr-5">
+          <img
+            src="${values.medium_cover_image}"
+            alt="${values.title}"
+            class="w-full border-4 border-white mb-2"
+          />
+          <button
+            class="w-full bg-green-500 rounded py-2 mb-2 text-white text-xl font-bold"
+          >
+            <i class="fa-solid fa-download text-black"></i>
+            Download
+          </button>
+          <button
+            class="w-full bg-cyan-400 rounded py-2 text-white text-xl font-bold"
+          >
+            Watch Now
+          </button>
+        </div>
+        <div class="w-2/4 px-12 ml-6">
+          <h1 class="text-4xl text-white font-bold pb-5">
+          ${values.title}
+          </h1>
+          <p class="text-white text-sm font-bold">${values.year}</p>
+          <p class="text-white font-bold">${values.genres}</p>
+          <div class="flex items-center">
+            <p class="text-white font-bold italic mr-2">Available in:</p>
+            <p class="text-white text-xs border-[1px] border-white px-2 mr-2">
+              2160p.WEB
+            </p>
+          </div>
+          <p class="text-white text-sm my-2 font-semibold">
+            WEB: same quality as BlueRay, but ripped earlier from a streaming
+            service
+          </p>
+          <p class="text-white text-sm mb-4 font-semibold">
+            BlueRay estimated Date:
+            <span class="text-green-500 font-bold pl-2">July 04,2022</span>
+          </p>
+          <button
+            class="rounded py-2 px-4 text-white text-xs border-[1px] border-white font-bold my-4"
+          >
+            <i class="fa-solid fa-download text-green-500 pr-2"></i>
+            Request Subtitles
+          </button>
+          <div class="flex items-center">
+            <i class="fa-solid fa-heart text-green-500 text-2xl mr-8"></i>
+            <p class="text-white text-xl font-bold px-2 mr-2">4</p>
+          </div>
+          <div class="flex items-center">
+            <i class="fa-solid fa-popcorn"></i>
+            <i class="fa-solid fa-heart text-green-500 text-2xl mr-8"></i>
+            <p class="text-white text-xl font-bold px-2 mr-2">86% Critics</p>
+          </div>
+          <div class="flex items-center">
+            <i class="fa-solid fa-people-group text-green-500 text-2xl mr-8"></i>
+            <p class="text-white text-xl font-bold px-2 mr-2">97% - Audience</p>
+          </div>
+          <div class="flex items-center">
+            <i class="fa-solid fa-heart text-green-500 text-2xl mr-8"></i>
+            <p class="text-white text-xl font-bold px-2 mr-2">
+              7.7<i class="fa-solid fa-star text-green-500 text-xl pb-3 ml-2"></i>
+            </p>
+          </div>
+          <div class="flex items-center mt-8">
+            <p class="text-white font-bold italic mr-2">Keyboards:</p>
+            <p
+              class="text-white font-bold text-xs border-[1px] border-white rounded px-4 py-2 mr-2"
+            >
+              period drama
+            </p>
+            <p
+              class="text-white font-bold text-xs border-[1px] border-white rounded px-4 py-2 mr-2"
+            >
+              country house
+            </p>
+            <p
+              class="text-white font-bold text-xs border-[1px] border-white rounded px-4 py-2 mr-2"
+            >
+              britian
+            </p>
+          </div>
+        </div>
+        <div class="w-1/4">
+          <p class="pl-3 pb-3 text-xl text-white font-bold">Similar Movies</p>
+          <div class="flex mb-3">
+            <img
+              class="border-4 border-white ml-3 w-48 h-48"
+              src="https://img.yts.mx/assets/images/movies/northanger_abbey_2007/medium-cover.jpg"
+              alt="movies"
+            />
+            <img
+              class="border-4 border-white ml-3 w-48 h-48"
+              src="https://img.yts.mx/assets/images/movies/northanger_abbey_2007/medium-cover.jpg"
+              alt="movies"
+            />
+          </div>
+          <div class="flex">
+            <img
+              class="border-4 border-white ml-3 w-48 h-48"
+              src="https://img.yts.mx/assets/images/movies/northanger_abbey_2007/medium-cover.jpg"
+              alt="movies"
+            />
+            <img
+              class="border-4 border-white ml-3 w-48 h-48"
+              src="https://img.yts.mx/assets/images/movies/northanger_abbey_2007/medium-cover.jpg"
+              alt="movies"
+            />
+          </div>
+        </div>
+      </section>
+      <section class="mx-48 my-8 flex">
+        <div class="relative z-1 w-1/3">
+          <iframe
+            src="https://www.youtube.com/embed/r9jwGansp1E"
+            class="w-full h-full"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <i
+            class="fa-solid fa-circle-play text-white absolute top-1/2 text-6xl left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+          ></i>
+        </div>
+  
+        <div class="w-1/3">
+          <img
+            class="w-full h-full"
+            src="https://img.yts.mx/assets/images/movies/Men_in_Black_1997/medium-screenshot2.jpg"
+            alt="trailer"
+          />
+        </div>
+        <div class="w-1/3">
+          <img
+            class="w-full h-full"
+            src="https://img.yts.mx/assets/images/movies/Men_in_Black_1997/medium-screenshot2.jpg"
+            alt="movie"
+          />
+        </div>
+      </section>
+      <section class="mx-48 my-8 flex">
+        <div class="w-2/3 mr-12">
+          <p class="pb-3 text-xl text-white font-bold">Plot Summary</p>
+          <p class="text-base text-[#919191] mb-12">
+            ${values.summary}
+          </p>
+          <p class="text-white text-sm my-2 italic font-semibold">
+            Uploaded By: OTTO
+          </p>
+          <span class="text-white text-sm font-bold italic"
+            >${values.date_uploaded}</span
+          >
+        </div>
+        <div class="w-1/3 pl-12">
+          <div>
+            <p class="pb-3 text-xl text-white font-bold">Director</p>
+            <div class="flex items-center border-b border-white-500 pb-3">
+              <img
+                src="https://img.yts.mx/assets/images/actors/thumb/nm0001756.jpg"
+                class="rounded-full mr-3"
+                alt="director"
+              />
+              <p
+                class="text-[#919191] hover:text-white hover:font-semibold text-sm"
+              >
+                Barry Sonnenfeld
+              </p>
+            </div>
+          </div>
+          <div>
+            <p class="pb-3 text-xl text-white font-bold mt-8">Top Casts</p>
+            <div class="flex items-center border-b border-white-500 pb-3">
+              <img
+                src="https://img.yts.mx/assets/images/actors/thumb/nm0001756.jpg"
+                class="rounded-full mr-3"
+                alt="director"
+              />
+              <p
+                class="text-[#919191] hover:text-white hover:font-semibold text-sm"
+              >
+                Barry Sonnenfeld
+              </p>
+            </div>
+            <div class="flex items-center border-b border-white-500 pb-3 mt-2">
+              <img
+                src="https://img.yts.mx/assets/images/actors/thumb/nm0001756.jpg"
+                class="rounded-full mr-3"
+                alt="director"
+              />
+              <p
+                class="text-[#919191] hover:text-white hover:font-semibold text-sm"
+              >
+                Barry Sonnenfeld
+              </p>
+            </div>
+            <div class="flex items-center border-b border-white-500 pb-3 mt-2">
+              <img
+                src="https://img.yts.mx/assets/images/actors/thumb/nm0001756.jpg"
+                class="rounded-full mr-3"
+                alt="director"
+              />
+              <p
+                class="text-[#919191] hover:text-white hover:font-semibold text-sm"
+              >
+                Barry Sonnenfeld
+              </p>
+            </div>
+            <div class="flex items-center border-b border-white-500 pb-3 mt-2">
+              <img
+                src="https://img.yts.mx/assets/images/actors/thumb/nm0001756.jpg"
+                class="rounded-full mr-3"
+                alt="director"
+              />
+              <p
+                class="text-[#919191] hover:text-white hover:font-semibold text-sm"
+              >
+                Barry Sonnenfeld
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="mx-48 my-12 flex">
+        <div class="w-1/2">
+          <p class="pb-3 text-xl text-white font-bold">
+            <i class="fa-solid fa-star text-green-500 text-xl pr-2"></i> 5
+            comments
+          </p>
+          <div class="flex items-center justify-between p-2 mt-2 bg-black mr-8">
+            <div class="flex items-center pb-3 mt-2">
+              <img
+                src="https://img.yts.mx/assets/images/users/thumb/61666861639828782.jpg"
+                class="rounded-full mr-3"
+                alt="director"
+              />
+              <div class="pr-12">
+                <p
+                  class="text-[#919191] hover:text-white hover:font-semibold text-sm"
+                >
+                  turq82 April 02, 2022 at 09:55 am
+                </p>
+                <p class="text-white font-semibold text-sm">Men to slap</p>
+              </div>
+            </div>
+            <div class="flex items-center">
+              <p class="text-white text-base font-bold">
+                5 <i class="fa-solid fa-heart text-green-500 text-2xl"></i>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-1/2 ml-8">
+          <p class="pb-3 text-xl text-white font-bold">
+            <i class="fa-solid fa-star text-green-500 text-xl pr-2"></i> Plot
+            Summary
+          </p>
+          <p class="text-[#919191] text-sm my-2 font-semibold">
+            Reviewed by
+            <span class="text-white text-sm font-bold">The LightSongbird</span>
+            <i class="fa-solid fa-star text-green-500 text-base pr-2"></i>
+            <span class="text-white text-xs font-bold">9/10</span>
+          </p>
+  
+          <p class="text-base text-[#919191] mb-2">
+            Men in Black is great fun from start to finish. If you care to look
+            past the rather ridiculous plot (only occasionally), this is hugely
+            enjoyable. The cinematography is skillful likewise with the direction,
+            and the special effects are wonderful. The script is irreverent, and
+            there are some truly great performances. Will Smith proves here he CAN
+            act, and Tommy Lee Jones as usual is marvellous. Both make a unique
+            and hilarious double act. Then there is fine support from underrated
+            actor Rip Torn and from Linda Fiorentino, who is a talented actress
+            that hasn't earned herself a high acting profile yet. The finale is a
+            little overblown, but you cannot deny this is a fun film.
+          </p>
+          <p class="text-base text-[#919191]">Read More</p>
+        </div>
+      </section>`;
+    });
+   document.getElementById("detailPage").innerHTML=data1;
+    
+}).catch((error) =>{
+    console.log("Unable to fetch data",error);
+})
